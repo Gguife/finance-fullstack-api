c5b856db3518c7fd3442ce4e54c8c13f5526ecfa
Gguife <gguife747@gmail.com>
2024-02-02 17:22:08 -0300

    feat: Add getByEmail model

--- src/module/user/user.model.js
+++ src/module/user/user.model.js
@@ -1,21 +1,37 @@
+import bcrypt from "bcrypt";
 import knex from "../../service/knex.js";
 
+const TABLE = "users";
+
 export const getAll = () =>{
-  return knex("users");
+  return knex(TABLE).select(
+    'id', 
+    'name', 
+    'email', 
+    'created_at', 
+    'updated_at'
+    );
 }
 
 export const get = (id) =>{
-  return knex("users").where({id}).first();
+  return knex(TABLE)
+  .where({id}).select('id','name','email','created_at','updated_at')
+  .first();
 }
 
 export const save = (params) =>{
-  return knex("users").insert(params);
+  params.password = bcrypt.hashSync(params.password, 10);
+  return knex(TABLE).insert(params);
 }
 
 export const remove = (id) =>{
-  return knex("users").delete(id)
+  return knex(TABLE).delete(id)
 }
 
 export const update = (id, params) =>{
-  return knex("users").where({id}).update(params);
+  return knex(TABLE).where({id}).update(params);
+}
+
+export const getByEmail = (email) =>{
+  return knex(TABLE).where({email}).first();
 }
