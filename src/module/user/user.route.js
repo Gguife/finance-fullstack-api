8874403faf947f7c68bf1e9b4190e2bd39ff48ef
Gguife <gguife747@gmail.com>
2024-02-02 17:21:42 -0300

    fix: Update method

--- src/module/user/user.route.js
+++ src/module/user/user.route.js
@@ -18,14 +18,15 @@ router.post("/", async (req, res) =>{
   return res.status(200).json({data})
 })
 
+router.put("/:id", async (req, res) =>{
+  const data = await update(req.params.id, req.body);
+  return res.status(200).json({data})
+})
+ 
 router.delete("/:id", async (req, res) =>{
   const data = await remove(req.params.id);
   return res.status(200).json({data})
 })
 
-router.put("/:id", async (req, res) =>{
-  const data = await update(req.params.id, req.body);
-  return res.status(200).json({data})
-}) 
 
 export default router;
