c3aa90ba311b0fc6b3aff2b07fd4b8c3dbfb03f3
Gguife <gguife747@gmail.com>
2024-02-05 14:58:54 -0300

    feat: Create register sevice and your its rules

--- src/module/auth/auth.service.js
+++ src/module/auth/auth.service.js
@@ -21,4 +21,15 @@ export const login = async (params) => {
   return { token };
 }
 
-export const register = async (params) => {}
+export const register = async (params) => { 
+  const user = await getByEmail(params.email);
+  
+  if(user){
+    return {error: "This email already exists"};
+  }
+
+  const userCreated = await save(params);
+
+  const token = jwt.sign({ id: userCreated[0] }, process.env.JWT_SECRET);
+  return { token };  
+}
