649defc660286f88d7adfbf4a22584ba857e68e6
Gguife <gguife747@gmail.com>
2024-02-06 15:32:38 -0300

    feat: Config cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 dotenv.config();
 
 import useRouter from "./module/user/user.route.js";
@@ -7,6 +8,7 @@ import authRouter from "./module/auth/auth.route.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", useRouter);
 app.use("/auth", authRouter);
