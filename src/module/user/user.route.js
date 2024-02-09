3e5ffabb60f22f2f43643028baf0bdc644f45ae6
Gguife <gguife747@gmail.com>
2024-02-09 17:41:49 -0300

    feat: Create getMe route and your service

--- src/module/user/user.route.js
+++ src/module/user/user.route.js
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import { getAll, get, save, update, remove } from "./user.model.js";
+import { getMe } from "./user.service.js"; 
 
 const router = Router();
 
 router.get("/", async (_, res) =>{
   const data = await getAll();
+  return res.status(200).json({data})
+})
+
+router.get("/me", async (req, res) =>{
+  const data = await getMe(req.token);
+  if(data.error){
+    return res.status(400).json({error: data.error});
+  }
+
   return res.status(200).json({data})
 })
 
