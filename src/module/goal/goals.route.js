470fe82afb1db13def75fd2ba1ec5d89f5b77c4f
Gguife <gguife747@gmail.com>
2024-02-15 16:23:20 -0300

    feat: Created goals model and your routes

--- src/module/goal/goals.route.js
+++ src/module/goal/goals.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAll, get, save, update, remove } from "./metas.model.js";
+import { getAll, get, save, update, remove } from "./goals.model.js";
 import { authMiddleware } from "../../middleware/authMiddleware.js";
 
 const router = Router();
