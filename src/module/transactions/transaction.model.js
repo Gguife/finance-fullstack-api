2f31a5765772ddc75953cca2d558565ad3bd62b6
Gguife <gguife747@gmail.com>
2024-02-20 16:19:54 -0300

    feat: Created transactions module

---
+++ src/module/transactions/transaction.model.js
@@ -0,0 +1,33 @@
+import knex from "../../service/knex.js";
+
+const TABLE = "transactions";
+
+export const getAll = (userId) =>{
+  return knex(TABLE).select("*").where({"user_id": userId});
+}
+
+export const get = (id, userId) =>{
+  return knex(TABLE)
+  .where({id})
+  .andWhere({"user_id": userId})
+  .select("*")
+  .first();
+}
+
+export const save = (params) =>{
+  return knex(TABLE).insert(params);
+}
+
+export const update = (id, params, userId) =>{
+  return knex(TABLE)
+  .where({id})
+  .andWhere({"user_id": userId})
+  .update(params);
+}
+
+export const remove = (id, userId) =>{
+  return knex(TABLE)
+  .where({id})
+  .andWhere({"user_id": userId})
+  .delete();
+}
